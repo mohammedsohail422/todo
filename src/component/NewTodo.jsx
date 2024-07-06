0fc09c15392644fa3e24886ce1051e70059c57eb
mohammed sohail <93545017+mohammedsohail422@users.noreply.github.com>
2024-07-06 18:17:07 +0530

    Create NewTodo.jsx
    new todo

---
+++ src/component/NewTodo.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const NewTodo = ()=>{
+  const [item, setItem] = useState("");
+  const [lists, setLists] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  
+  const handleAdd =()=>{
+    if(!item){
+      alert("enter something")
+    }else if(item && editIndex !== null){
+      const updateLists = lists.map((ele, ind)=>(ind === editIndex ? item : ele))
+      setLists(updateLists)
+      setEditIndex(null);
+      setItem("");
+    }else{
+      setLists([...lists, item])
+      setItem("")
+    }
+  }
+const handleDel =(index)=>{
+  const updatedEle = lists.filter((elements, id)=>{
+    return index !== id;
+  })
+  setLists(updatedEle);
+}
+const deleteAll = ()=>{
+  setLists([])
+}
+const handleEdit = (index)=>{
+  setItem(lists[index]);
+    setEditIndex(index);
+}
+return(
+  <>
+    <h1>Todo <button onClick={deleteAll}>Clear all</button></h1>
+    <input type="text" value={item} onChange={(e)=>setItem(e.target.value)}/>
+    <button onClick={handleAdd}>Add</button>
+   {
+     lists.map((ele,index)=>{
+       return(
+        <div key={index}>
+          <div>
+            <span>{ele}</span>
+            <button onClick={()=>handleEdit(index)}>Edit</button>
+            <button onClick={()=>handleDel(index)}>Delete</button>
+           </div>
+        </div>
+       )
+     })
+   }
+  </>
+)
+}
+export default NewTodo;
